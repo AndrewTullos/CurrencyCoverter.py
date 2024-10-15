88e78998be629b7f5b40adb24136cd1fef04fe74
Drew <tullos.andrew@gmail.com>
2024-10-15 10:54:35 -0500

    Control flow added

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,13 +1,18 @@
 def run_Converter():
    while True:
+      print("What currency are you starting with? ")
+      choices = ["US Dollar", "Euro", "British Pound", "CA Dollar", "Yen", "Franc", "Peso"]
+      
+
       start_curr = input("Enter amount: ")
 
       try:
          start_curr = float(start_curr)
+
          if start_curr >= 0:
-            print(f"You entered {start_curr}. The converted amount is {converted_curr}")
+            print(f"You entered {start_curr}. The converted amount is {converted_curr}.")
          else:
-            print("Amount cannot be negative")
+            print("Amount cannot be negative.")
             run_Converter()
       except ValueError:
          print("Invalid amount")
