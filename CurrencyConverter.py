c89ec15f0f793ac15719941d6eaac3d2982f181d
Drew <tullos.andrew@gmail.com>
2024-10-15 10:47:45 -0500

    Control flow added

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,3 +1,18 @@
+def run_Converter():
+   while True:
+      start_curr = input("Enter amount: ")
 
-step_1 = input("Enter amount: ")
-print("Invalid amount")
+      try:
+         start_curr = float(start_curr)
+         if start_curr >= 0:
+            print(f"You entered {start_curr}. The converted amount is {converted_curr}")
+         else:
+            print("Amount cannot be negative")
+            run_Converter()
+      except ValueError:
+         print("Invalid amount")
+
+def converted_curr():
+   print('test')
+
+run_Converter()
