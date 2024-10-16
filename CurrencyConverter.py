c0747ac50a0006a82188651000bba0510b62c74e
Drew <tullos.andrew@gmail.com>
2024-10-16 11:05:02 -0500

    Logic added

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,23 +1,61 @@
-def run_Converter():
+from prompt_toolkit import PromptSession
+from prompt_toolkit.completion import WordCompleter
+import currency_converter
+
+choices = ["USD", "EUR", "GBP", "JPY", "CHF", "MXN", "CAD"]
+completer = WordCompleter(choices)
+
+def run_converter():
    while True:
-      print("What currency are you starting with? ")
-      choices = ["US Dollar", "Euro", "British Pound", "CA Dollar", "Yen", "Franc", "Peso"]
+      # Select starting currency
+      print("\n *** Menu *** \nChoose from the following options below:")
+      print(choices)
+      session = PromptSession()
+      result = session.prompt("What is your starting currency? ", completer=completer)
+
+                  
+      if result not in choices:
+         print("Invalid currency choice.")
+         continue
+
+
+      # Select converted currency
+      session_two = PromptSession()
+      result_two = session_two.prompt("What is your new desired currency? ", completer=completer)
+
+      if result not in choices:
+         print("Invalid currency choice.")
+         continue
       
+      while True:
+         # Select numerical value of currency
+         start_curr = input("Enter amount: ")
+         try:
+            start_curr = float(start_curr)
+            # Genereates numerical conversion of currency
+            if start_curr >= 0:
+               break            
+            else:
+               print("Amount cannot be negative. Please enter a positive value.")
+         except ValueError:
+            print("Invalid amount. Please enter a number.")
+      
+      c = currency_converter.CurrencyConverter()
+      converted_curr = c.convert(start_curr, result, result_two)
+      print(f"You entered {start_curr} {result}. The converted amount is {converted_curr} {result_two}.")
 
-      start_curr = input("Enter amount: ")
+      
+      def run_again():
+         close_app = input("Would you like to convert another amount? (Y/N)").upper
 
-      try:
-         start_curr = float(start_curr)
-
-         if start_curr >= 0:
-            print(f"You entered {start_curr}. The converted amount is {converted_curr}.")
+         if close_app == "Y":
+            run_converter()
+         elif roll == "N":
+            print("See you later!")
+            exit()
          else:
-            print("Amount cannot be negative.")
-            run_Converter()
-      except ValueError:
-         print("Invalid amount")
+            print("Invalid choice!")
+            run_again()
+         
 
-def converted_curr():
-   print('test')
-
-run_Converter()
+run_converter()
